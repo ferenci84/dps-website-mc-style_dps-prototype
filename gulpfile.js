59af4777fbdbbc6ae36c4db2e61418b13b468824
Zoltan Laszlo Ferenci <ferenci84@gmail.com>
2018-08-28 11:29:36 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,22 @@ var postcssdiscarddublicates = require('postcss-discard-duplicates');
 var cssimport = require('postcss-import');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
+var runSequence = require('run-sequence');
 
 gulp.task('postcss', function() {
     return gulp.src(['styles/*.css'])
         .pipe(postcss([cssimport,cssvariables({preserve:true}),postcssdiscarddublicates,postcssclean]))
-        .pipe(gulp.dest('out/styles'));
+        .pipe(gulp.dest('out/styles'))
+        .pipe(gulp.dest('../DPSWebsite/web/styles'))
 });
 
-gulp.task('watch_dev_css', ['postcss'], function() {
-    return watch('styles/*.css', function(event) {
-        console.log("update postcss");
-        return gulp.start('postcss');
-    });
+gulp.task('copy_original_styles', function() {
+    return gulp.src(['styles/*.css','styles/*.less','styles/*.scss','styles/*.css.map'])
+        .pipe(gulp.dest('../DPSWebsite/web/styles-dev'))
+});
+
+gulp.task('process_css', function(cb){
+    runSequence(['postcss','copy_original_styles'],function() {
+        cb();
+    })
 });
